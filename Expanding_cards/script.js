5d7d59f1445d6fb46c6c59903a4600d19475a030
Uniquezee <priscaonyemaobiamarachi@gmail.com>
2023-03-10 22:34:20 -0500

    new update

--- Expanding_cards/script.js
+++ Expanding_cards/script.js
@@ -1,14 +1,18 @@
 const panels = document.querySelectorAll('.panel')
+const panelH3Elem = document.getElementById('panelH3')
 
 panels.forEach((panel) => {
     panel.addEventListener('click', () => {
         removeActiveClasses()
         panel.classList.add('active')
+        panelH3Elem.classList.add('description')
     })
 })
 
 function removeActiveClasses(){
     panels.forEach(panel => {
         panel.classList.remove('active')
+        panelH3Elem.classList.remove('description')
+
     })
 }
